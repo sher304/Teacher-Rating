e2a40f5ee50f41e28adc03eb42d5f378bf3c7bfc
sher304 <esherovshermat@gmail.com>
2023-03-09 23:33:04 +0600

    :lipstick: Done Loign Page

--- TeacherRatingProject/TeacherRatingProject/View/MainVC.swift
+++ TeacherRatingProject/TeacherRatingProject/View/MainVC.swift
@@ -67,8 +67,24 @@ class MainVC: UIViewController {
         textF.layer.borderWidth = 0.2
         textF.autocorrectionType = .no
         textF.autocapitalizationType = .none
-        
         return textF
+    }()
+    
+    private lazy var loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .customDarkGreen
+        button.setTitle("Войти", for: .normal)
+        button.layer.cornerRadius = 8
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
+    private lazy var registrationButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Зарегистрироваться", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 20)
+        return button
     }()
     
     override func viewDidLoad() {
@@ -134,6 +150,19 @@ class MainVC: UIViewController {
             make.top.equalTo(passwordLabel.snp.bottom).offset(3)
         }
         
+        mainInfromV.addSubview(loginButton)
+        loginButton.snp.makeConstraints { make in
+            make.leading.equalTo(20)
+            make.trailing.equalTo(-20)
+            make.top.equalTo(passwordField.snp.bottom).offset(60)
+            make.height.equalTo(50)
+        }
+        
+        view.addSubview(registrationButton)
+        registrationButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(mainInfromV.snp.bottom).offset(27)
+        }
         
     }
     

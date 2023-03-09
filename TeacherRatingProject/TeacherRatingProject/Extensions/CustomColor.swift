5b70022183f96913eb3e92b32fc5131c16c02081
sher304 <esherovshermat@gmail.com>
2023-03-09 23:22:36 +0600

    :lipstick: Add Main UI

---
+++ TeacherRatingProject/TeacherRatingProject/Extensions/CustomColor.swift
@@ -0,0 +1,19 @@
+//
+//  CustomColor.swift
+//  TeacherRatingProject
+//
+//  Created by Шермат Эшеров on 09/03/2023.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    static let beige = UIColor(red: 225/255, green: 235/255, blue: 206/255, alpha: 1)
+    static let customGreen = UIColor(red: 96/255, green: 108/255, blue: 56/255, alpha: 1)
+    static let tintBeige = UIColor(red: 254/255, green: 250/255, blue: 224/255, alpha: 1)
+    static let customDarkGreen
+    = UIColor(red: 53/255, green: 72/255, blue: 30/255, alpha: 1)
+
+}
--- TeacherRatingProject/TeacherRatingProject/View/MainVC.swift
+++ TeacherRatingProject/TeacherRatingProject/View/MainVC.swift
@@ -6,13 +6,137 @@
 //
 
 import UIKit
+import SnapKit
 
 class MainVC: UIViewController {
-
+    
+    private lazy var beigeBGImage: UIImageView = {
+        let imageV = UIImageView()
+        imageV.image = UIImage(named: "Vector 42")
+        return imageV
+    }()
+    
+    private lazy var logoImage: UIImageView = {
+        let imageV = UIImageView()
+        imageV.image = UIImage(named: "Group 33")
+        return imageV
+    }()
+    
+    private lazy var greetingText: UILabel = {
+        let label = UILabel()
+        label.text = "Рады видеть Вас снова!"
+        label.font = .systemFont(ofSize: 19)
+        label.textColor = UIColor.customGreen
+        return label
+    }()
+    
+    private lazy var mainInfromV: UIView = {
+        let view = UIView()
+        view.backgroundColor = .tintBeige
+        view.layer.cornerRadius = 8
+        return view
+    }()
+    
+    private lazy var userNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Имя Пользователя"
+        label.font = .systemFont(ofSize: 16)
+        return label
+    }()
+    
+    private lazy var userNameField: UITextField = {
+        let textF = UITextField()
+        textF.layer.cornerRadius = 8
+        textF.layer.borderColor = UIColor.black.cgColor
+        textF.layer.borderWidth = 0.2
+        textF.autocorrectionType = .no
+        textF.autocapitalizationType = .none
+        return textF
+    }()
+    
+    private lazy var passwordLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Пароль"
+        return label
+    }()
+    
+    private lazy var passwordField: UITextField = {
+        let textF = UITextField()
+        textF.layer.cornerRadius = 8
+        textF.layer.borderColor = UIColor.black.cgColor
+        textF.layer.borderWidth = 0.2
+        textF.autocorrectionType = .no
+        textF.autocapitalizationType = .none
+        
+        return textF
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupconstraints()
     }
-
+    
+    private func setupconstraints(){
+        view.backgroundColor = .customDarkGreen
+        
+        view.addSubview(beigeBGImage)
+        beigeBGImage.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.height.equalTo(293)
+        }
+        
+        view.addSubview(logoImage)
+        logoImage.snp.makeConstraints { make in
+            make.leading.equalTo(20)
+            make.top.equalTo(57)
+            
+        }
+        
+        view.addSubview(greetingText)
+        greetingText.snp.makeConstraints { make in
+            make.leading.equalTo(logoImage.snp.leading)
+            make.top.equalTo(logoImage.snp.bottom).offset(10)
+        }
+        
+        view.addSubview(mainInfromV)
+        mainInfromV.snp.makeConstraints { make in
+            make.leading.equalTo(20)
+            make.trailing.equalTo(-20)
+            make.top.equalTo(greetingText.snp.bottom).offset(10)
+            make.height.equalTo(623)
+        }
+        
+        mainInfromV.addSubview(userNameLabel)
+        userNameLabel.snp.makeConstraints { make in
+            make.leading.equalTo(20)
+            make.top.equalTo(20)
+        }
+        
+        mainInfromV.addSubview(userNameField)
+        userNameField.snp.makeConstraints { make in
+            make.leading.equalTo(userNameLabel.snp.leading)
+            make.trailing.equalTo(-20)
+            make.height.equalTo(50)
+            make.top.equalTo(userNameLabel.snp.bottom).offset(3)
+        }
+        
+        mainInfromV.addSubview(passwordLabel)
+        passwordLabel.snp.makeConstraints { make in
+            make.leading.equalTo(userNameLabel.snp.leading)
+            make.top.equalTo(userNameField.snp.bottom).offset(36)
+        }
+        
+        mainInfromV.addSubview(passwordField)
+        passwordField.snp.makeConstraints { make in
+            make.leading.equalTo(passwordLabel.snp.leading)
+            make.trailing.equalTo(-20)
+            make.height.equalTo(50)
+            make.top.equalTo(passwordLabel.snp.bottom).offset(3)
+        }
+        
+        
+    }
+    
 }
+
 

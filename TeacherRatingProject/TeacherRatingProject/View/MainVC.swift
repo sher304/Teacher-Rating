203394b38d31f41939ace85d90a6a19848b41e04
sher304 <esherovshermat@gmail.com>
2023-03-11 20:09:05 +0600

    :lipstick: Done Main VC with logo, and profile buttons

--- TeacherRatingProject/TeacherRatingProject/View/MainVC.swift
+++ TeacherRatingProject/TeacherRatingProject/View/MainVC.swift
@@ -24,6 +24,12 @@ class MainVC: UIViewController {
         return button
     }()
     
+    private lazy var logoImage: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "Group 33")
+        return image
+    }()
+    
     private lazy var mainImage: UIImageView = {
         let image = UIImageView()
         image.image = UIImage(named: "students")
@@ -31,6 +37,14 @@ class MainVC: UIViewController {
         return image
     }()
     
+    private lazy var profileButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "person.circle"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.imageEdgeInsets = UIEdgeInsets(top: 30, left: 30, bottom: 30, right: 30)
+        button.tintColor = .gray
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,10 +61,22 @@ class MainVC: UIViewController {
         
         firstBGView.addSubview(burgerMenu)
         burgerMenu.snp.makeConstraints { make in
-            make.height.equalTo(35)
-            make.width.equalTo(35)
+            make.height.width.equalTo(35)
             make.leading.equalTo(20)
             make.top.equalTo(firstBGView.safeAreaLayoutGuide).offset(10)
+        }
+        
+        firstBGView.addSubview(logoImage)
+        logoImage.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalTo(burgerMenu)
+        }
+        
+        firstBGView.addSubview(profileButton)
+        profileButton.snp.makeConstraints { make in
+            make.trailing.equalTo(-20)
+            make.centerY.equalTo(logoImage)
+            make.height.width.equalTo(35)
         }
         
         firstBGView.addSubview(mainImage)
